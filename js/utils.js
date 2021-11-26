918331398da4ffa1748e92c9a363504ca2f2fcfc
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-11-26 17:06:21 +0100

    added star rating

--- js/utils.js
+++ js/utils.js
@@ -1,58 +1,60 @@
-import { oneStar } from "../media/ratings/svg.js";
+import { fiveStar, fourStar, oneStar, threeStar, twoStar, zeroStar } from "../media/ratings/svg.js";
+
 
 export const fetchProducts = async (url) => {
     const response = await fetch(url);
     return await response.json();
 };
 
+const starRating = (rating) => {
+    switch(rating) {
+        case 0:
+            return (
+                zeroStar
+            )
+        
+        case 1:
+            return (
+                oneStar
+            )
+
+        case 2:
+            return (
+                twoStar
+            )
+
+        case 3:
+            return (
+                threeStar
+            )   
+
+        case 4:
+            return (
+                fourStar
+            )
+
+        case 5:
+            return (
+                fiveStar
+            )
+
+    }
+}
+
 export const showProducts = (item) => {
     const { img_url, title, price, rating } = item;
-
-    const starRating = (rating) => {
-        switch(rating) {
-            case 0:
-                return (
-                    oneStar
-                )
-            
-            case 1:
-                return (
-                    "two"
-                )
-
-            case 2:
-                return (
-                    "three"
-                )
-
-            case 3:
-                return (
-                    "four"
-                )   
-
-            case 4:
-                return (
-                    "five"
-                )
-
-            case 5:
-                return (
-                    "null"
-                )
-
-        }
-    }
-
 
     return `
         <div class="card">
             <img src="${img_url}" alt="${title}">
             <h4>${title}</h4>
             <p>£ ${price}</p>
-            <p>${starRating(rating)}
+            <p>${starRating(rating)}</p>
             <button>Add to cart</button>
         </div>
     `
 
     
 }
+
+
