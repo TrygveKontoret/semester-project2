6080b599a4f316fbf29af3a3b60473aeddb851d8
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-11-26 16:08:31 +0100

    displayed featured content on site

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 import { url } from './api.js';
-import { fetchProducts } from './utils.js';
+import { fetchProducts, showProducts } from './utils.js';
 
 console.log(await fetchProducts(url));
 
 const container = document.querySelector('.container');
 
+let data = []
+
+const getProducts = async () => {
+    const products = await fetchProducts(url);
+    for (const product of products) {
+        data.push(product)
+    };
+    render()
+};
+
+const render = () => {
+    container.innerHTML = '';
+
+    for (const product of data) {
+        if (product.featured) {
+        container.innerHTML += showProducts(product);
+        }
+    }
+}
+
+getProducts();
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,58 @@
+import { oneStar } from "../media/ratings/svg.js";
 
 export const fetchProducts = async (url) => {
     const response = await fetch(url);
     return await response.json();
 };
+
+export const showProducts = (item) => {
+    const { img_url, title, price, rating } = item;
+
+    const starRating = (rating) => {
+        switch(rating) {
+            case 0:
+                return (
+                    oneStar
+                )
+            
+            case 1:
+                return (
+                    "two"
+                )
+
+            case 2:
+                return (
+                    "three"
+                )
+
+            case 3:
+                return (
+                    "four"
+                )   
+
+            case 4:
+                return (
+                    "five"
+                )
+
+            case 5:
+                return (
+                    "null"
+                )
+
+        }
+    }
+
+
+    return `
+        <div class="card">
+            <img src="${img_url}" alt="${title}">
+            <h4>${title}</h4>
+            <p>£ ${price}</p>
+            <p>${starRating(rating)}
+            <button>Add to cart</button>
+        </div>
+    `
+
+    
+}
