92ff6a5bbbfa744d7bef4a1b0af52810a83e31df
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-12-11 18:46:50 +0100

    finished login, and created admin page

--- js/login.js
+++ js/login.js
@@ -2,43 +2,66 @@ import { url } from './api.js';
 import { saveToken, saveUser } from './utils.js';
 
 
-// async function login(username, password) {
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const form = document.querySelector('form');
+const errorMessage = document.querySelector('.errorMessage');
 
-//     const urlPass = url + "auth/local";
-//     const data = JSON.stringify({ identifier: username, password: password });
+const showMessage = (message, messageType, target) => {
+    const item = document.querySelector(target);
+    item.innerHTML = `
+    <div class_"${messageType}">${message}</div>
+    `;
+}
 
-//     const options = {
-//         method: "POST",
-//         body: data,
-//         headers: {
-//             "Content-Type": "application/json" 
-//         }
-//     }
+const submitForm = (e) => {
+    e.preventDefault();
+
+    errorMessage.innerHTML = '';
+
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue.length === 0 || passwordValue.length === 0) {
+        return showMessage("Please fill in the fields properly!", "Invalid", ".errorMessage")
+    }
+
+    login(usernameValue, passwordValue);
+}
 
 
-//     try {
-//         const response = await fetch(urlPass, options);
-//         const json = await response.json();
-//         console.log(json);
+form.addEventListener('submit', submitForm);
 
+const login = async (username, password) => {
 
-//         if (json.user) {
+    const urlPass = url + 'auth/local';
+    const data = JSON.stringify({ identifier: username, password: password});
 
-//             saveToken(json.jwt);
-//             saveUser(json.user)
+    const options = {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 
-//             location.href = "https://img.ifunny.co/images/045355f75e5d96f1125f75e5dc458cebed2c82996935f49d16d355fe2814d690_1.webp"
+    try {
+        const response = await fetch(urlPass, options);
+        const json = await response.json();
 
-//         }
+        if (json.user) {
+            saveToken(json.jwt);
+            saveUser(json.user);
 
-//         if (json.error) {
-//             showMessage("Wrong username and password combination", "invalid", ".message");
-//         }
+            location.href = './admin.html';
+        };
 
-//     }
+        if (json.error) {
+            showMessage("wrong username or password", "Invalid", ".errorMessage");
+        };
+    }
 
-//     catch(error) {
-//         showMessage("ERROR!", error, ".message");
-//     }
-
-// }
+    catch(error) {
+        showMessage("errorrr", error, ".errorMessage");
+    }
+};
